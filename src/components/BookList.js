baa4c51fbb185192af9f135f37f7186f0d8e7d68
Sambit Kumar Sutar <sambitsutar1999@gmail.com>
2024-09-24 01:28:22 +0530

    Update BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,4 @@
-// src/components/BookList.js
+
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchBooks, addToCart } from '../redux/BookSlice';
