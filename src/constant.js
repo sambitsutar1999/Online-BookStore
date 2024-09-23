d8f436ce15cb53503c50673660838d024bda33b3
Sambit Kumar Sutar <sambitsutar1999@gmail.com>
2024-09-24 01:23:32 +0530

    Update constant.js and remove unused files

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL;
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_URL = 'AIzaSyA9FZqVHwOzolLROKOX5AkAg2edC-OZ5P4';
+export const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
