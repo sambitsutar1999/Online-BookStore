268ddc9ad3203b6c82b90468490a5c4bb76f9f2a
Sambit Kumar Sutar <sambitsutar1999@gmail.com>
2024-09-24 11:40:34 +0530

    Updated .gitignore and Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,16 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAFXFXxrBRJYemMVu4F9s1OpSMRCA-7XQc",
-  authDomain: "book-store-d5ec2.firebaseapp.com",
-  projectId: "book-store-d5ec2",
-  storageBucket: "book-store-d5ec2.appspot.com",
-  messagingSenderId: "974169870609",
-  appId: "1:974169870609:web:8b4535f49ac2832fb4bcb0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-
 export const auth = getAuth(app);
+
